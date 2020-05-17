c0b68910b8595047bf75d274fe4f924f42aab330
Felipe Oduardo Sierra <felipe@solutionnine.com>
2020-05-17 14:07:06 -0600

    Replaces request with axios. Renames the plugin `homebridge-octoprint-plus`. Modifies the logic to allow pause/resume and cancelling through the brightness controls.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# homebridge-octoprint
+# homebridge-octoprint-plus
 
-Plugin to support [OctoPrint](https://octopi.octoprint.org) 3D printer control via the [HomeBridge Platform](https://github.com/nfarina/homebridge).
+Plugin to support [OctoPrint](https://octopi.octoprint.org) 3D printer control via the [HomeBridge Platform](https://github.com/nfarina/homebridge). Builds on the original [homebridge-octoprint]() by allowing pausing/resuming of prints, not just cancelling.
 
 # Installation
 
@@ -11,8 +11,8 @@ Plugin to support [OctoPrint](https://octopi.octoprint.org) 3D printer control v
 ```
 "accessories": [
     {
-      "accessory": "OctoPrint",
-      "name": "OctoPrint",
+      "accessory": "OctoPrintPlus",
+      "name": "PrinterName",
       "api_key": "XXXXX"
     }
 ]
@@ -20,12 +20,12 @@ Plugin to support [OctoPrint](https://octopi.octoprint.org) 3D printer control v
 
 # What does this plugin do?
 
-This plugin adds the current print status of OctoPrint to HomeKit. The print status is shown as a lightbulb, with the brightness value equaling the current print progress (0-100%). If you deactivate the lightbulb, octoprint is instructed to abort the current print. Note, that print jobs cannot be started with this plugin (so activating the lightbulb does not do anything).
+This plugin adds the current print status of OctoPrint to HomeKit. The print status is shown as a lightbulb, with the brightness value equaling the current print progress (0-100%). If you deactivate the lightbulb, OctoPrint is instructed to pause the current print. Activating the lightbulb will resume the print. Setting the lightbulb to 100% will cancel the print where it is.
 
 # Notes for implementation
 
 ## Possible Characteristics and Services
-Description of characteristics (available methods and how to build listener) can be found [here]( https://github.com/KhaosT/HAP-NodeJS/blob/master/lib/Characteristic.js). Characteristics hava a setValue() and a getValue() method.
+Description of characteristics (available methods and how to build listener) can be found [here]( https://github.com/KhaosT/HAP-NodeJS/blob/master/lib/Characteristic.js). Characteristics have a setValue() and a getValue() method.
 
 Overview of all available characteristics and services can be found [here](https://github.com/KhaosT/HAP-NodeJS/blob/master/lib/gen/HomeKitTypes.js).
 
@@ -35,4 +35,4 @@ When adding characteristics:
 
 ## Start in Developer Mode
 
-To start the plugin in developer mode run `homebridge -D -P . -U ~/.homebridge-dev/` while beeing in the root directory. A sample config has to be saved at `~/.homebridge-dev/`.
+To start the plugin in developer mode run `homebridge -D -P . -U ~/.homebridge-dev/` while being in the root directory. A sample config has to be saved at `~/.homebridge-dev/`.
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var Service, Characteristic;
-var ReqP = require("request-promise");
+var axios = require("axios");
 
-module.exports = function(homebridge){
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-    // registration of each accessory
-    homebridge.registerAccessory("homebridge-octoprint","OctoPrint",OctoPrint);
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  // registration of each accessory
+  homebridge.registerAccessory("homebridge-octoprint-plus", "OctoPrintPlus", OctoPrintPlus);
 }
 
 //**************************************************************************************************
@@ -18,7 +18,7 @@ module.exports = function(homebridge){
 // Bricklet Remote Switch
 //**************************************************************************************************
 
-function OctoPrint(log, config) {
+function OctoPrintPlus(log, config) {
   this.log = log;
 
   // parse config
@@ -26,129 +26,158 @@ function OctoPrint(log, config) {
   this.server = config["server"] || 'http://octopi.local';
   this.apiKey = config["api_key"];
 
-  log.info("Initialized OctoPrint Accessory at " + this.server);
+  log.info("Initialized OctoPrint Plus Accessory at " + this.server);
 }
 
-OctoPrint.prototype = {
-    getServices: function() {
+OctoPrintPlus.prototype = {
+  getServices: function() {
 
-        var informationService = new Service.AccessoryInformation();
-        informationService
-            .setCharacteristic(Characteristic.Manufacturer, "Guy Sheffer and the Community")
-            .setCharacteristic(Characteristic.Model, "OctoPrint");
+    var informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, "Guy Sheffer and the Community")
+      .setCharacteristic(Characteristic.Model, "OctoPrint");
 
-        var controlService = new Service.Lightbulb();
+    var controlService = new Service.Lightbulb();
 
-        controlService
-            .getCharacteristic(Characteristic.On)
-            .on('get', this.getPrintingState.bind(this))
-            .on('set', this.setPrintingState.bind(this));
+    controlService
+      .getCharacteristic(Characteristic.On)
+      .on('get', this.getPrintingState.bind(this))
+      .on('set', this.setPrintingState.bind(this));
 
-        controlService
-            .getCharacteristic(Characteristic.Brightness)
-            .on('get', this.getProgress.bind(this))
-            .on('set', this.setProgress.bind(this));
+    controlService
+      .getCharacteristic(Characteristic.Brightness)
+      .on('get', this.getProgress.bind(this))
+      .on('set', this.setProgress.bind(this));
 
-        // set name
-        controlService.setCharacteristic(Characteristic.Name, this.name);
+    // set name
+    controlService.setCharacteristic(Characteristic.Name, this.name);
 
-        return [informationService, controlService];
-    },
+    return [informationService, controlService];
+  },
 
-    // This function gets the current printing state (1 = printing, 0 = not printing)
-    getPrintingState(callback) {
-        this.log('Getting current printing state: GET ' + this.server + '/api/printer');
+  // This function gets the current printing state (1 = printing, 0 = not printing)
+  getPrintingState(callback) {
+    this.log('Getting current printing state: GET ' + this.server + '/api/printer');
 
-        var options = {
-            method: 'GET',
-            uri: this.server + '/api/printer',
-            headers: {
-                "X-Api-Key": this.apiKey
-            },
-            json: true
-        };
+    var options = {
+      method: 'GET',
+      uri: this.server + '/api/printer',
+      headers: {
+        "X-Api-Key": this.apiKey
+      },
+      json: true
+    };
 
-        ReqP(options).then(function(printState) {
-            var state = printState.state.flags.printing;
-            console.log("Printer is printing: " + state)
-            if (state == false) {
-                callback(null, 0);
-            } else {
-                callback(null, 1);
-            }
+    axios.request(options).then(function(printState) {
+        var state = printState.state.flags.printing;
+        console.log("Printer is printing: " + state)
+        if (state == false) {
+          callback(null, 0);
+        } else {
+          callback(null, 1);
+        }
+      })
+      .catch(function(error) {
+        callback(error);
+      });
+  },
+
+  setPrintingState(value, callback) {
+    if (value == 1) {
+      console.log("Resuming print.");
+      var options = {
+        method: 'POST',
+        uri: this.server + '/api/job',
+        headers: {
+          "X-Api-Key": this.apiKey
+        },
+        body: {
+          "command": "resume"
+        },
+        json: true
+      };
+      axios.request(options).then(function(printState) {
+          console.log("Print resumed successfully.")
+          callback(null);
         })
         .catch(function(error) {
-            callback(error);
+          callback(error);
         });
-    },
+    } else {
+      console.log("Pausing print.");
+      var options = {
+        method: 'POST',
+        uri: this.server + '/api/job',
+        headers: {
+          "X-Api-Key": this.apiKey
+        },
+        body: {
+          "command": "pause"
+        },
+        json: true
+      };
+      axios.request(options).then(function(printState) {
+          console.log("Print paused successfully.")
+          callback(null);
+        })
+        .catch(function(error) {
+          callback(error);
+        });
+    }
+  },
 
-    /*
-        This function sets the current printing state. It is only allowed to shut down the printer
-    */
-    setPrintingState(value, callback) {
-        if (value == 1) {
-            console.log("You cannot start a print with homekit.");
-            callback(1);
+  getProgress(callback) {
+    this.log('Getting current job data: GET ' + this.server + '/api/job');
+
+    var options = {
+      method: 'GET',
+      uri: this.server + '/api/job',
+      headers: {
+        "X-Api-Key": this.apiKey
+      },
+      json: true
+    };
+
+    axios.request(options).then(function(printState) {
+        var completion = printState.progress.completion;
+        if (completion == null) {
+          console.log("Printer currently not printing.")
+          callback(null, 0);
         } else {
-            console.log("Stopping print.");
-            var options = {
-                method: 'POST',
-                uri: this.server + '/api/job',
-                headers: {
-                    "X-Api-Key": this.apiKey
-                },
-                body: {
-                    "command": "cancel"
-                },
-                json: true
-            };
-            ReqP(options).then(function(printState) {
-                console.log("Print stopped successfully.")
-                callback(null);
-            })
-            .catch(function(error) {
-                callback(error);
-            });
+          console.log("Current completion: " + JSON.stringify(completion));
+          completionInt = Math.round(parseFloat(completion));
+          callback(null, completionInt);
         }
-    },
+      })
+      .catch(function(error) {
+        callback(error);
+      });
+  },
 
-    /*
-        This function gets the current progress of the print job and stores it as a
-        lightbulb's brightness value.
-    */
-    getProgress(callback) {
-       this.log('Getting current job data: GET ' + this.server + '/api/job');
-
-       var options = {
-           method: 'GET',
-           uri: this.server + '/api/job',
-           headers: {
-               "X-Api-Key": this.apiKey
-           },
-           json: true
-       };
-
-       ReqP(options).then(function(printState) {
-           var completion = printState.progress.completion;
-           if (completion == null) {
-               console.log("Printer currently not printing.")
-               callback(null, 0);
-           } else {
-               console.log("Current completion: " + JSON.stringify(completion));
-               completionInt = Math.round(parseFloat(completion));
-               callback(null, completionInt);
-           }
-       })
-       .catch(function(error) {
-           callback(error);
-       });
-   },
-
-   /*
-        Not allowed.
-   */
-   setProgress(value, callback) {
-       console.log("Cannot set progress with homekit!");
-       callback(1);
-   }
+  setProgress(value, callback) {
+    if (value === 100) {
+      console.log("Cancelling print.");
+      var options = {
+        method: 'POST',
+        uri: this.server + '/api/job',
+        headers: {
+          "X-Api-Key": this.apiKey
+        },
+        body: {
+          "command": "cancel"
+        },
+        json: true
+      };
+      axios.request(options).then(function(printState) {
+          console.log("Print cancelled successfully.")
+          callback(null);
+        })
+        .catch(function(error) {
+          callback(error);
+        });
+    } else {
+      console.log("Cannot set custom progress!");
+      callback(1);
+    }
+  }
 }
